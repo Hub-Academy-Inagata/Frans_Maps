a33a6208f160f34b4fdad96991d357646ab46727
frans <franswinata6@gmail.com>
2023-08-25 15:02:58 +0700

    (+) Detail Data

--- src/script.js
+++ src/script.js
@@ -1,12 +1,20 @@
 let isOpen = false;
 
+if (!isOpen) {
+  document.getElementById('detail-data').style.opacity = 0;
+  document.getElementById('detail-data').style.display = 'none';
+} else {
+  document.getElementById('detail-data').style.opacity = 1;
+  document.getElementById('detail-data').style.display = 'flex';
+}
+
 document.querySelectorAll('.allPaths').forEach((e) => {
   e.setAttribute('class', `allPaths ${e.id}`);
   e.addEventListener('mouseover', function () {
     window.onmousemove = function (j) {
       x = j.clientX;
       y = j.clientY;
-      document.getElementById('name').style.top = y - 150 + 'px';
+      document.getElementById('name').style.top = y - 180 + 'px';
       document.getElementById('name').style.left = x - 200 + 'px';
     };
     const classes = e.className.baseVal.replace(/ /g, '.');
@@ -14,10 +22,7 @@ document.querySelectorAll('.allPaths').forEach((e) => {
       //   country.style.fill = 'pink';
     });
     document.getElementById('name').style.opacity = 1;
-
-    // if (e.id === 'Wonosari') {
-    //   document.getElementById('namep').innerText = e.id;
-    // }
+    document.getElementById('namep').innerText = e.id;
   });
   e.addEventListener('mouseleave', function () {
     // const classes = e.className.baseVal.replace(/ /g, '.');
@@ -28,17 +33,16 @@ document.querySelectorAll('.allPaths').forEach((e) => {
   });
 
   e.addEventListener('click', () => {
-    if (isOpen) {
-      document.getElementById('detail-data').style.opacity = 0;
-      document.getElementById('detail-data').style.display = 'none';
-      isOpen = !isOpen;
-    } else {
-      document.getElementById('detail-data').style.opacity = 1;
-      document.getElementById('detail-data').style.display = 'block';
-      isOpen = !isOpen;
-    }
+    document.getElementById('kecamatan').innerText = e.id;
+    isOpen = true;
+    document.getElementById('detail-data').style.opacity = 1;
+    document.getElementById('detail-data').style.display = 'flex';
   });
 });
 
-document.getElementById('detail-data').style.opacity = 0;
-document.getElementById('detail-data').style.display = 'none';
+document
+  .getElementById('arrow-right')
+  .addEventListener('click', () => {
+    document.getElementById('detail-data').style.opacity = 0;
+    document.getElementById('detail-data').style.display = 'none';
+  });
